55a91523890d87f2bdcd64e965e4b1631995ea48
Tesafye Atomsa <tatomsa@student.cccs.edu>
2020-03-18 17:21:30 -0600

    second commit to check repo

---
+++ CSC237_tatomsa_Challenge1/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CSC237_tatomsa_Challenge1.Models;
+
+namespace CSC237_tatomsa_Challenge1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+       
+    }
+}
